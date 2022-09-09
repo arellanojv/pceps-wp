deafcc34568f445d36b8ba3b9cad0cd1e7e112c4
Jhon Arellano <jah4n@Jhons-MacBook-Air.local>
2022-09-09 21:14:18 +0800

    Refactor page container within account page / add tabs

--- src/pages/account/login/index.js
+++ src/pages/account/login/index.js
@@ -1,20 +1,82 @@
-import { Link, Router } from '@gatsbyjs/reach-router'
+import { Link, Router, useMatch } from '@gatsbyjs/reach-router'
+import Page from '../../../components/Page'
 
 import LoginForm from './login-form'
 import RegisterForm from './register-form'
 
 const Login = () => {
+  const matchSignIn = useMatch('/account')
+  const matchCreateAccount = useMatch('/account/create-account')
+
   return (
-    <div className='section login'>
-      <nav className='tab-nav'>
-        <Link to=''>Sign in</Link>
-        <Link to='create-account'>Create Account</Link>
-      </nav>
-      <Router primary={false}>
-        <LoginForm path='/' />
-        <RegisterForm path='/create-account' />
-      </Router>
-    </div>
+    <Page title='Account'>
+      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-md w-full space-y-8'>
+          <div>
+            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+              Sign in to your account
+            </h2>
+          </div>
+          <div className='section login'>
+            <div class='text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700'>
+              <ul class='flex flex-wrap -mb-px'>
+                <li class='mr-2'>
+                  {matchSignIn ? (
+                    <Link
+                      to=''
+                      class='inline-block p-4 text-orange-600 rounded-t-lg border-b-2 border-orange-600 active dark:text-orange-500 dark:border-orange-500'
+                    >
+                      Sign in
+                    </Link>
+                  ) : (
+                    <Link
+                      to=''
+                      class='inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                </li>
+                <li class='mr-2'>
+                  {matchCreateAccount ? (
+                    <Link
+                      to='create-account'
+                      class='inline-block p-4 text-orange-600 rounded-t-lg border-b-2 border-orange-600 active dark:text-orange-500 dark:border-orange-500'
+                    >
+                      Create Account
+                    </Link>
+                  ) : (
+                    <Link
+                      to='create-account'
+                      class='inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                    >
+                      Create Account
+                    </Link>
+                  )}
+
+                  {/* <a
+                    href='#'
+                    class='inline-block p-4 text-orange-600 rounded-t-lg border-b-2 border-orange-600 active dark:text-orange-500 dark:border-orange-500'
+                    aria-current='page'
+                  >
+                    Create Account
+                  </a> */}
+                </li>
+              </ul>
+            </div>
+            {/* <nav className='tab-nav'>
+              <Link to=''>Sign in</Link>
+              <Link to='create-account'>Create Account</Link>
+            </nav> */}
+
+            <Router primary={false}>
+              <LoginForm path='/' />
+              <RegisterForm path='/create-account' />
+            </Router>
+          </div>
+        </div>
+      </div>
+    </Page>
   )
 }
 
--- src/pages/account/login/register-form.js
+++ src/pages/account/login/register-form.js
@@ -30,95 +30,82 @@ export const RegisterForm = () => {
   }
 
   return (
-    <Page title='Register'>
-      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='max-w-md w-full space-y-8'>
-          <div>
-            <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
-              Register
-            </h2>
+    <form onSubmit={onRegister} className='mt-8 space-y-6'>
+      {(error || passwordError) && (
+        <div className='error-notice'>
+          <div
+            className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'
+            role='alert'
+          >
+            <span className='block sm:inline'>{passwordError || error}</span>
           </div>
-          <form onSubmit={onRegister} className='mt-8 space-y-6'>
-            {(error || passwordError) && (
-              <div className='error-notice'>
-                <div
-                  className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative'
-                  role='alert'
-                >
-                  <span className='block sm:inline'>
-                    {passwordError || error}
-                  </span>
-                </div>
-              </div>
-            )}
-            <div className='rounded-md shadow-sm -space-y-px'>
-              <div className='border-0'>
-                <div className='mb-6'>
-                  <label
-                    htmlFor='email'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    Username
-                  </label>
-                  <input
-                    onChange={(e) => setUsername(e.target.value)}
-                    id='email'
-                    name='email'
-                    type='text'
-                    autoComplete='email'
-                    className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                    placeholder='Your username'
-                  />
-                </div>
+        </div>
+      )}
+      <div className='rounded-md shadow-sm -space-y-px'>
+        <div className='border-0'>
+          <div className='mb-6'>
+            <label
+              htmlFor='email'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Username
+            </label>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              id='email'
+              name='email'
+              type='text'
+              autoComplete='email'
+              className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+              placeholder='Your username'
+            />
+          </div>
 
-                <div className='mb-6'>
-                  <label
-                    htmlFor='email'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    Email
-                  </label>
-                  <input
-                    onChange={(e) => setEmail(e.target.value)}
-                    id='email'
-                    name='email'
-                    type='text'
-                    autoComplete='email'
-                    className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                    placeholder='you@example.com'
-                  />
-                </div>
-                <div className='mb-6'>
-                  <label
-                    htmlFor='password'
-                    className='block mb-2 text-sm font-medium text-gray-900'
-                  >
-                    Password
-                  </label>
-                  <input
-                    onChange={(e) => setPassword(e.target.value)}
-                    type='password'
-                    id='password'
-                    autoComplete='password'
-                    className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-                    placeholder='Your password'
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <button
-                type='submit'
-                className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
-              >
-                Create Account
-              </button>
-            </div>
-          </form>
+          <div className='mb-6'>
+            <label
+              htmlFor='email'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              id='email'
+              name='email'
+              type='text'
+              autoComplete='email'
+              className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+              placeholder='you@example.com'
+            />
+          </div>
+          <div className='mb-6'>
+            <label
+              htmlFor='password'
+              className='block mb-2 text-sm font-medium text-gray-900'
+            >
+              Password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type='password'
+              id='password'
+              autoComplete='password'
+              className='border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+              placeholder='Your password'
+            />
+          </div>
         </div>
       </div>
-    </Page>
+
+      <div>
+        <button
+          type='submit'
+          className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
+        >
+          Create Account
+        </button>
+      </div>
+    </form>
   )
 }
 
